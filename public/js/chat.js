93aa568db7bba319247ed4b7619ec54f855fa865
Ankit <ankit@farmguide.in>
2018-09-10 18:46:28 +0530

    add users list and update users

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,28 @@
 var socket = io();
-// socket.emit('createMessage', {
-// 	from: 'User',
-// 	text: 'hello'
-// }, function() {
-// 	console.log('got it');
-// })
+socket.on('connect', function() {
+	var params = $.deparam(window.location.search);
+	socket.emit('join', params, function(err) {
+		if(err) {
+			alert(err);
+			window.location.href = '/';
+		} else {
+			console.log('no error');
+		}
+	})
+})
+
+function scrollToBottom() {
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child');
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = messages.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+	if(scrollTop + clientHeight + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
 
 socket.on('newMessage', function(data) {
 	var formattedTime = moment(data.createdAt).format('h:mm a');
@@ -20,6 +38,7 @@ socket.on('newMessage', function(data) {
 		from: data.from
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 })
 
 socket.on('newLocationMessage', function(message) {
@@ -31,11 +50,26 @@ socket.on('newLocationMessage', function(message) {
 		from: message.from
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 })
 
 socket.on('disconnect', function(data) {
 	console.log('disconnected')
+	
+
 })
+
+socket.on('updateUserList', function(users) {
+	console.log(users);
+	var ol = $('<ol></ol>')
+	users.forEach(function(user) {
+		ol.append($('<li></li>').text(user));
+	})
+
+	$('#users').html(ol)
+})
+
+
 
 socket.on('joininMessage', function(data) {
 	console.log('data')
--- server/server.js
+++ server/server.js
@@ -6,21 +6,20 @@ const express = require('express');
 const port = process.env.PORT || 3100;
 var app = express();
 var server = http.createServer(app);
+const {Users} = require('./utils/users');
 var io = socketIO(server);
 var os = require("os")
 app.use(express.static(publicPath));
 
 var {generateMessage, generateLocationMessage} = require('./utils/message');
-
+const {isRealString} = require('./utils/validation');
 
 // app.get('/', (req, res) => {
 // 	res.send('Up and running');
 // })
-
+var users = new Users();
 io.on('connection', (socket) => {
-	socket.emit('newMessage', generateMessage('Admin', 'Welcome ' + os.userInfo().username))
-
-	socket.broadcast.emit('newMessage',generateMessage('Admin', os.userInfo().username + ' joined the group'))
+	
 
 	socket.on('createMessage', (data, callback) => {
 		console.log('show Data', data)
@@ -30,10 +29,32 @@ io.on('connection', (socket) => {
 
 	socket.on('disconnect', function(data) {
 		console.log('disconnected')
+		var user = users.removeUser(socket.id);
+		if(user) {
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+		}
 	})
 
 	socket.on('locationData', (pos) => {
 		io.emit('newLocationMessage', generateLocationMessage('Admin', pos.lat, pos.lng));
+	})
+
+	socket.on('join', (params, callback) => {
+		if(!isRealString(params.name) || !isRealString(params.room)) {
+			callback('Name and room name are required');
+			return;
+		}
+
+		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		socket.emit('newMessage', generateMessage('Admin', 'Welcome ' + os.userInfo().username))
+
+		socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin', params.name + ' has joined.'))
+		//socket.leave(params.room)
+		callback();
 	})
 
 
