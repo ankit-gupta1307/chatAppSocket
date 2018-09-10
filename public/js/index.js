4fdc69ade9f887b34b1f402c89168e429fb9f710
Ankit <ankit@farmguide.in>
2018-09-10 16:04:24 +0530

    moment and time added to frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,24 @@
 var socket = io();
-socket.emit('createMessage', {
-	from: 'Ankit',
-	text: 'hello'
-}, function() {
-	console.log('got it');
-})
+// socket.emit('createMessage', {
+// 	from: 'User',
+// 	text: 'hello'
+// }, function() {
+// 	console.log('got it');
+// })
 
 socket.on('newMessage', function(data) {
+	var formattedTime = moment(data.createdAt).format('h:mm a');
 	console.log('show Data', data)
 	var li = $('<li></li>');
-	li.text(`${data.from}: ${data.text}`);
+	li.text(`${data.from} ${formattedTime}: ${data.text}`);
 	$('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function(message) {
+	var formattedTime = moment(data.createdAt).format('h:mm a');
 	var li = $('<li></li>');
 	var a = $('<a target="_blank">My current location</a>');
-	li.text = `${message.from}:`
+	li.text = `${message.from} ${formattedTime}:`
 	a.attr('href', message.url);
 	li.append(a);
 	$('#messages').append(li);
@@ -32,11 +34,12 @@ socket.on('joininMessage', function(data) {
 
 $("#message-form").on('submit', function(e) {
 	e.preventDefault();
+	var messageTextBox = $('[name=message]');
 	socket.emit('createMessage', {
-		from: 'Ankit',
-		text: $('[name=message]').val()
+		from: 'User',
+		text: messageTextBox.val()
 	}, function() {
-		console.log('got it');
+		messageTextBox.val('');
 	})
 })
 
@@ -44,13 +47,15 @@ $('#sendLocation').on('click', function() {
 	if(!navigator.geolocation) {
 		return alert("Geolocation not supported by your browser");
 	}
-
+	$('#sendLocation').attr('disabled', 'disabled');
 	navigator.geolocation.getCurrentPosition(function(pos) {
+		$('#sendLocation').removeAttr('disabled');
 	 	socket.emit('locationData', {
 	 		lat: pos.coords.latitude,
 	 		lng: pos.coords.longitude
 	 	})
 	}, function(err) {
+		$('#sendLocation').removeAttr('disabled');
 		alert('Unable to fetch location');
 	})
 })
